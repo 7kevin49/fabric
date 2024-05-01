3d3c816e9d8556375d4671df9cdf1dbbd326db1a
Giles Heron <1226267+gilesheron@users.noreply.github.com>
2024-05-01 22:46:29 +0100

    point at API correctly from webui

--- installer/server/webui/fabric_web_server.py
+++ installer/server/webui/fabric_web_server.py
@@ -29,7 +29,7 @@ def send_request(prompt, endpoint):
         KeyError: If the response JSON does not contain the expected "response" key.
     """
 
-    base_url = "http://0.0.0.0:13337"
+    base_url = "http://127.0.0.1:13337"
     url = f"{base_url}{endpoint}"
     headers = {
         "Content-Type": "application/json",
